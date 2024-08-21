b85b01945eb434f74667997b5903c123a30c442b
codexsabir <sbr9997@gmail.com>
2024-08-21 17:27:49 +0500

    updating cv

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,8 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
+
+  
   let year = date.getFullYear();
   return (
     <Container fluid className="footer">
